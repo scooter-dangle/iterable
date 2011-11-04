d3895f91807076597a56d9ecc096239e72ce573a
scooter-dangle <scottlsteele@gmail.com>
2011-11-04 00:43:52 +0000

    Renamed iterable_array.rb to iterable.rb (and similar for its _spec.rb)

--- iterable_spec.rb
+++ iterable_spec.rb
@@ -1,10 +1,11 @@
-require 'iterable_array'
+require '~/iterable/iterable.rb'
 
 describe IterableArray do
 	it 'should not claim to be an Array' do
 		@itArray = IterableArray.new
 		@itArray.class.should_not == Array
 	end
+
 	it 'should respond to every Array instance method' do # MAYbe...
 		@itArray = IterableArray.new
 		Array.instance_methods.each do |method|
