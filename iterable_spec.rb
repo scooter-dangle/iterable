16a7afa9f36f83b49c503eeba48d5b9d490b6933
scooter-dangle <scottlsteele@gmail.com>
2011-11-05 14:23:24 -0400

    Replaced stupid tabs with 4 spaces

--- iterable_spec.rb
+++ iterable_spec.rb
@@ -1,15 +1,15 @@
 require '~/iterable/iterable.rb'
 
 describe IterableArray do
-	it 'should not claim to be an Array' do
-		@itArray = IterableArray.new
-		@itArray.class.should_not == Array
-	end
+    it 'should not claim to be an Array' do
+        @itArray = IterableArray.new
+        @itArray.class.should_not == Array
+    end
 
-	it 'should respond to every Array instance method' do # MAYbe...
-		@itArray = IterableArray.new
-		Array.instance_methods.each do |method|
-			@itArray.should respond_to(method)
-		end
-	end
+    it 'should respond to every Array instance method' do # MAYbe...
+        @itArray = IterableArray.new
+        Array.instance_methods.each do |method|
+            @itArray.should respond_to(method)
+        end
+    end
 end
