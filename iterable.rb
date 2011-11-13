229660cf9535aa3edbef1834e5b4a4ce9b79f0d8
scooter-dangle <scottlsteele@gmail.com>
2011-11-12 19:13:45 -0500

    Simplify :&
    Previously had a condition that checked the type
    of the argument. Not sure why that was necessary.

--- iterable.rb
+++ iterable.rb
@@ -8,7 +8,7 @@ class IterableArray
     # @@plain_accessors contains methods that return a non-array without
     # modifying the called array.
 
-    @@plain_accessors = [ :==, :size, :length, :to_a, :to_s, :include?, :hash, :to_ary, :inspect, :at, :empty? ]
+    @@plain_accessors = [ :==, :size, :length, :to_a, :to_s, :include?, :hash, :to_ary, :inspect, :at, :reverse, :empty? ]
 
     # @@special_accessors contains non-modifying methods that (would otherwise)
     # return an array. For this class, we want to return IterableArray objects
@@ -29,7 +29,7 @@ class IterableArray
     @@hybrids   = [ :fetch, :fill, :index ]
 
     # The following two lines are supposed to help me keep track of progress.
-    # working:  Array#public_instance_methods => [ :frozen?, :[]=, :concat, :<<, :shift, :unshift, :insert, :reverse_each, :find_index, :rindex, :reverse, :reverse!, :rotate, :rotate!, :sort, :sort!, :sort_by!, :select!, :keep_if, :delete_if, :reject, :reject!, :zip, :transpose, :replace, :clear, :<=>, :slice, :slice!, :|, :uniq, :uniq!, :compact, :compact!, :flatten, :flatten!, :shuffle!, :shuffle, :sample, :cycle, :permutation, :repeated_permutation, :repeated_combination, :product, :take, :take_while, :drop, :pack, :entries, :sort_by, :grep, :find, :detect, :find_all, :flat_map, :collect_concat, :inject, :reduce, :partition, :group_by, :all?, :any?, :one?, :none?, :min, :max, :minmax, :min_by, :max_by, :minmax_by, :member?, :each_with_index, :each_entry, :each_slice, :each_cons, :each_with_object, :chunk, :slice_before, :nil?, :===, :=~, :!~, :clone, :dup, :freeze, :tap, :extend, :display, :to_enum, :enum_for, :!, :!= ]
+    # working:  Array#public_instance_methods => [ :frozen?, :[]=, :concat, :<<, :shift, :unshift, :insert, :reverse_each, :find_index, :rindex, :reverse!, :rotate, :rotate!, :sort, :sort!, :sort_by!, :select!, :keep_if, :delete_if, :reject, :reject!, :zip, :transpose, :replace, :clear, :<=>, :slice, :slice!, :|, :uniq, :uniq!, :compact, :compact!, :flatten, :flatten!, :shuffle!, :shuffle, :sample, :cycle, :permutation, :repeated_permutation, :repeated_combination, :product, :take, :take_while, :drop, :pack, :entries, :sort_by, :grep, :find, :detect, :find_all, :flat_map, :collect_concat, :inject, :reduce, :partition, :group_by, :all?, :any?, :one?, :none?, :min, :max, :minmax, :min_by, :max_by, :minmax_by, :member?, :each_with_index, :each_entry, :each_slice, :each_cons, :each_with_object, :chunk, :slice_before, :nil?, :===, :=~, :!~, :clone, :dup, :freeze, :tap, :extend, :display, :to_enum, :enum_for, :!, :!= ]
     # original: Array#public_instance_methods => [ :inspect, :to_s, :to_a, :to_ary, :frozen?, :==, :eql?, :hash, :[], :[]=, :at, :fetch, :first, :last, :concat, :<<, :push, :pop, :shift, :unshift, :insert, :each, :each_index, :reverse_each, :length, :size, :empty?, :find_index, :index, :rindex, :join, :reverse, :reverse!, :rotate, :rotate!, :sort, :sort!, :sort_by!, :collect, :collect!, :map, :map!, :select, :select!, :keep_if, :values_at, :delete, :delete_at, :delete_if, :reject, :reject!, :zip, :transpose, :replace, :clear, :fill, :include?, :<=>, :slice, :slice!, :assoc, :rassoc, :+, :*, :-, :&, :|, :uniq, :uniq!, :compact, :compact!, :flatten, :flatten!, :count, :shuffle!, :shuffle, :sample, :cycle, :permutation, :combination, :repeated_permutation, :repeated_combination, :product, :take, :take_while, :drop, :drop_while, :pack, :entries, :sort_by, :grep, :find, :detect, :find_all, :flat_map, :collect_concat, :inject, :reduce, :partition, :group_by, :all?, :any?, :one?, :none?, :min, :max, :minmax, :min_by, :max_by, :minmax_by, :member?, :each_with_index, :each_entry, :each_slice, :each_cons, :each_with_object, :chunk, :slice_before, :nil?, :===, :=~, :!~, :class, :singleton_class, :clone, :dup, :initialize_dup, :initialize_clone, :taint, :tainted?, :untaint, :untrust, :untrusted?, :trust, :freeze, :methods, :singleton_methods, :protected_methods, :private_methods, :public_methods, :instance_variables, :instance_variable_get, :instance_variable_set, :instance_variable_defined?, :instance_of?, :kind_of?, :is_a?, :tap, :send, :public_send, :respond_to?, :respond_to_missing?, :extend, :display, :method, :public_method, :define_singleton_method, :__id__, :object_id, :to_enum, :enum_for, :equal?, :!, :!=, :instance_eval, :instance_exec, :__send__ ]
 
     # The following 2 lines don't work in 1.8.7/1.9.2... I need to look up using
@@ -124,8 +124,7 @@ class IterableArray
             # end
 
             def &(arg)
-                return IterableArray.new(@array & arg) if arg.class == IterableArray or arg.class == Array
-                @array & arg
+                IterableArray.new(@array & arg.to_a)
             end
 
             def +(arg)
