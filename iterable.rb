91b5f20c4ac53e403f00c1065bb32011301faef4
scooter-dangle <scottlsteele@gmail.com>
2011-11-28 06:31:49 -0500

    Some more small additions

--- iterable.rb
+++ iterable.rb
@@ -5,11 +5,11 @@ class IterableArray
 
     # attr_accessor :array  # For testing purposes only!
 
-    @@plain_accessors = [ :==, :[]=, :size, :length, :to_a, :to_s, :include?, :hash, :to_ary, :inspect, :at, :reverse, :empty? ]
+    @@plain_accessors = [ :==, :[]=, :size, :length, :to_a, :to_s, :to_enum, :include?, :hash, :to_ary, :inspect, :at, :reverse, :empty? ]
     @@special_accessors = [ :&, :|, :*, :+, :-, :[], :<=>, :eql?, :indices, :indexes, :values_at, :join, :assoc, :rassoc, :first, :last ]
 
-    @@special_modifiers = [ :<< ]
     @@plain_modifiers = [ :delete, :delete_at, :pop, :push ]
+    @@special_modifiers = [ :<< ]
 
     @@iterators = [ :each, :collect, :collect!, :map, :map!, :combination, :count, :cycle, :delete_if, :drop_while, :each_index, :select ]
 
@@ -19,7 +19,7 @@ class IterableArray
     @@hybrids   = [ :fetch, :fill, :index ]
 
     # The following two lines are supposed to help me keep track of progress.
-    # working:  Array#public_instance_methods => [ :frozen?, :concat, :shift, :unshift, :insert, :reverse_each, :find_index, :rindex, :reverse!, :rotate, :rotate!, :sort, :sort!, :sort_by!, :select!, :keep_if, :delete_if, :reject, :reject!, :zip, :transpose, :replace, :clear, :slice, :slice!, :uniq, :uniq!, :compact, :compact!, :flatten, :flatten!, :shuffle!, :shuffle, :sample, :cycle, :permutation, :repeated_permutation, :repeated_combination, :product, :take, :take_while, :drop, :pack, :entries, :sort_by, :grep, :find, :detect, :find_all, :flat_map, :collect_concat, :inject, :reduce, :partition, :group_by, :all?, :any?, :one?, :none?, :min, :max, :minmax, :min_by, :max_by, :minmax_by, :member?, :each_with_index, :each_entry, :each_slice, :each_cons, :each_with_object, :chunk, :slice_before, :nil?, :===, :=~, :!~, :clone, :dup, :freeze, :tap, :extend, :display, :to_enum, :enum_for, :!, :!= ]
+    # working:  Array#public_instance_methods => [ :frozen?, :concat, :shift, :unshift, :insert, :reverse_each, :find_index, :rindex, :reverse!, :rotate, :rotate!, :sort, :sort!, :sort_by!, :select!, :keep_if, :delete_if, :reject, :reject!, :zip, :transpose, :replace, :clear, :slice, :slice!, :uniq, :uniq!, :compact, :compact!, :flatten, :flatten!, :shuffle!, :shuffle, :sample, :permutation, :repeated_permutation, :repeated_combination, :product, :take, :take_while, :drop, :pack, :entries, :sort_by, :grep, :find, :detect, :find_all, :flat_map, :collect_concat, :inject, :reduce, :partition, :group_by, :all?, :any?, :one?, :none?, :min, :max, :minmax, :min_by, :max_by, :minmax_by, :member?, :each_with_index, :each_entry, :each_slice, :each_cons, :each_with_object, :chunk, :slice_before, :nil?, :===, :=~, :!~, :clone, :dup, :freeze, :tap, :extend, :display, :enum_for, :!, :!= ]
     # original: Array#public_instance_methods => [ :inspect, :to_s, :to_a, :to_ary, :frozen?, :==, :eql?, :hash, :[], :[]=, :at, :fetch, :first, :last, :concat, :<<, :push, :pop, :shift, :unshift, :insert, :each, :each_index, :reverse_each, :length, :size, :empty?, :find_index, :index, :rindex, :join, :reverse, :reverse!, :rotate, :rotate!, :sort, :sort!, :sort_by!, :collect, :collect!, :map, :map!, :select, :select!, :keep_if, :values_at, :delete, :delete_at, :delete_if, :reject, :reject!, :zip, :transpose, :replace, :clear, :fill, :include?, :<=>, :slice, :slice!, :assoc, :rassoc, :+, :*, :-, :&, :|, :uniq, :uniq!, :compact, :compact!, :flatten, :flatten!, :count, :shuffle!, :shuffle, :sample, :cycle, :permutation, :combination, :repeated_permutation, :repeated_combination, :product, :take, :take_while, :drop, :drop_while, :pack, :entries, :sort_by, :grep, :find, :detect, :find_all, :flat_map, :collect_concat, :inject, :reduce, :partition, :group_by, :all?, :any?, :one?, :none?, :min, :max, :minmax, :min_by, :max_by, :minmax_by, :member?, :each_with_index, :each_entry, :each_slice, :each_cons, :each_with_object, :chunk, :slice_before, :nil?, :===, :=~, :!~, :class, :singleton_class, :clone, :dup, :initialize_dup, :initialize_clone, :taint, :tainted?, :untaint, :untrust, :untrusted?, :trust, :freeze, :methods, :singleton_methods, :protected_methods, :private_methods, :public_methods, :instance_variables, :instance_variable_get, :instance_variable_set, :instance_variable_defined?, :instance_of?, :kind_of?, :is_a?, :tap, :send, :public_send, :respond_to?, :respond_to_missing?, :extend, :display, :method, :public_method, :define_singleton_method, :__id__, :object_id, :to_enum, :enum_for, :equal?, :!, :!=, :instance_eval, :instance_exec, :__send__ ]
 
     @@plain_accessors.each { |meth| def_delegator :@array, meth }
@@ -215,7 +215,7 @@ class IterableArray
             end
 
             def map
-                return dup unless block_given?
+                return @array.dup unless block_given?
                 out = Array.new []
 
                 bastardize
@@ -255,11 +255,38 @@ class IterableArray
                 end
 
                 debastardize
-
                 self
             end
 
             alias_method :collect!, :map!
+
+            def cycle(n = nil, &block)
+                return @array.to_enum(:cycle, n) unless block_given?
+
+                bastardize
+
+                if n.equal? nil
+                    until @array.empty?
+                        @iter_index = 0
+                        while @iter_index < @array.size
+                            yield @array.at(@iter_index)
+                            @iter_index += 1
+                        end
+                    end
+                else
+                    n.times do
+                        @iter_index = 0
+                        while @iter_index < @array.size
+                            yield @array.at(@iter_index)
+                            @iter_index += 1
+                        end
+                    end
+                end
+
+                debastardize
+                nil
+            end
+
         end
     end
 
