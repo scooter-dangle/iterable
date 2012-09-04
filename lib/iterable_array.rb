921dbc4208186e2376bf347376bdced2e3a3e5d8
scooter-dangle <scottlsteele@gmail.com>
2012-09-04 11:49:42 +0100

    Fixed infinite loop

--- lib/iterable_array.rb
+++ lib/iterable_array.rb
@@ -818,9 +818,7 @@ class IterableArray
                 return @array.to_enum(:permutation, n) unless block_given?
 
                 catch_a_break do
-                    # following line is just to test to see if it's hanging
-                    @backward_index, @current_index, @forward_index = -1, 0, 1
-                    set = generate_permutations self, n
+                    set = generate_permutations self.to_a.dup, n
                     set.each do |item|
                         yield item
                     end
@@ -831,9 +829,7 @@ class IterableArray
                 return @array.to_enum(:combination, n) unless block_given?
 
                 catch_a_break do
-                    # following line is just to test to see if it's hanging
-                    @backward_index, @current_index, @forward_index = -1, 0, 1
-                    set = generate_combinations self, n
+                    set = generate_combinations self.to_a.dup, n
                     set.each do |item|
                         yield item
                     end
@@ -843,13 +839,13 @@ class IterableArray
             private
             def generate_permutations ary, n
                 out = []
-                ary.permutation n do |item|; out << item end
+                ary.permutation n do |item| out << item end
                 out
             end
 
             def generate_combinations ary, n
                 out = []
-                ary.combination n do |item|; out << item end
+                ary.combination n do |item| out << item end
                 out
             end
         end
