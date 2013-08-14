5860e3c31beb3eb966201e6d02fe5b42beaa6d9a
scooter-dangle <scottlsteele@gmail.com>
2013-08-13 22:26:50 -0400

    Add Iterable::Aliases
    Provides a way (Iterable::Aliases::Array) to still access the external
    Array class even after defining the Iterable::Array module.

---
+++ lib/iterable/aliases.rb
@@ -0,0 +1,7 @@
+module Iterable
+    module Aliases
+        # Need to be able to still refer to the external Array class
+        # even after defining the Iterable::Array module.
+        Array = [].class
+    end
+end
